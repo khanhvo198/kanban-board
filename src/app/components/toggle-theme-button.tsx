ee146c892d12057beb5d3c913dcbb3053a1f1894
MyStic <khanh.votuan@hcmut.edu.vn>
2024-10-13 20:41:13 +0700

    feat: add toggle theme button

---
+++ src/app/components/toggle-theme-button.tsx
@@ -0,0 +1,36 @@
+import { Icon, useColorMode, useColorModeValue } from "@chakra-ui/react"
+import { AnimatePresence } from "framer-motion"
+import { CiDark, CiLight } from "react-icons/ci"
+import { motion } from "framer-motion"
+
+
+export const ToggleThemeButton = () => {
+  const { toggleColorMode } = useColorMode()
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        key={useColorModeValue('light', 'dark')}
+      >
+        <Icon
+          as={useColorModeValue(CiDark, CiLight)}
+          fontSize={26}
+          onClick={toggleColorMode}
+          display="flex"
+          alignItems="center"
+          mr={2}
+          color="#787486"
+          strokeWidth="0.7"
+          strokeMiterlimit="10"
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          strokeOpacity="0.5"
+          _hover={{ cursor: 'pointer' }}
+        />
+      </motion.div>
+    </AnimatePresence>
+  )
+}
