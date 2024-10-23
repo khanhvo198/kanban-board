a398406c2b1c7c03f2bf14210237ccded51ca134
MyStic <khanh.votuan@hcmut.edu.vn>
2024-10-23 22:44:18 +0700

    feat: add react-dnd package

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,16 @@
 "use client"
-import { ChakraProvider } from "@chakra-ui/react"
-import theme from "./lib/theme"
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./lib/theme";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  return (
+    <ChakraProvider theme={theme}>
+      <DndProvider backend={HTML5Backend}>
+        {children}
+      </DndProvider>
+    </ChakraProvider>
+  )
 }
