b451637c7f6c81c51fce7cd70190a9b50fa47b91
MyStic <khanh.votuan@hcmut.edu.vn>
2024-11-05 10:50:09 +0700

    feat: implement UI for comment tab

--- src/app/components/comments-tab.tsx
+++ src/app/components/comments-tab.tsx
@@ -1,9 +1,67 @@
-import { Box } from "@chakra-ui/react"
+import { Avatar, Box, Input, InputGroup, InputRightElement, List, ListIcon, ListItem, Stack, Text } from "@chakra-ui/react"
+import { FC, useEffect, useRef, useState } from "react"
+import { PiPaperPlaneTiltThin } from "react-icons/pi"
+
+
+interface CommentState {
+  id: number,
+  comment: string
+}
+
+interface CommentMessageProps {
+  isAuthor: boolean
+}
+
+const CommentMessage: FC<CommentMessageProps> = ({ isAuthor }) => {
+  return (
+    <ListItem
+      display="flex"
+      flexDirection={isAuthor ? "row-reverse" : "row"}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Avatar size="sm" />
+      <Box border="1px solid #868686" p={2} w="90%" borderRadius="5px">
+        <Text>Test comment</Text>
+      </Box>
+    </ListItem>
+  )
+}
 
 export const CommentsTab = () => {
+  const bottomBoxRef = useRef<HTMLDivElement>(null)
+
+  const scrollToBottom = () => {
+    bottomBoxRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [])
+
   return (
-    <Box>
-      Comments Tab
-    </Box>
+    <Stack>
+      <List mt={5} spacing={3} pb="50px">
+        <CommentMessage isAuthor={false} />
+        <CommentMessage isAuthor={false} />
+        <CommentMessage isAuthor={false} />
+        <CommentMessage isAuthor={true} />
+        <CommentMessage isAuthor={true} />
+        <CommentMessage isAuthor={false} />
+        <CommentMessage isAuthor={false} />
+        <CommentMessage isAuthor={false} />
+        <CommentMessage isAuthor={true} />
+        <CommentMessage isAuthor={true} />
+      </List>
+      <Box ref={bottomBoxRef} />
+      <Box position="fixed" bottom="10px" left="10px" width="93%" backdropFilter="blur(10px)">
+        <InputGroup display="flex">
+          <Input borderColor="#868686" />
+          <InputRightElement>
+            <PiPaperPlaneTiltThin />
+          </InputRightElement>
+        </InputGroup>
+      </Box>
+    </Stack>
   )
 }
