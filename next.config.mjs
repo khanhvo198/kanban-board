7df3530b91b249f4c4116ee4fc5352f76013f1ed
MyStic <khanh.votuan@hcmut.edu.vn>
2024-11-07 20:39:32 +0700

    chore: add config for for RAM occupied issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    // Important: return the modified config
+    return config
+  },
+};
 
 export default nextConfig;
