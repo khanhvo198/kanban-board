935419d6d03653d97395901b48c9775061974c87
MyStic <khanh.votuan@hcmut.edu.vn>
2024-11-18 21:11:39 +0700

    feat: add projects route into protected routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { MainLayout } from "@/components/layout/main";
 import { useAuthStore } from "@/stores/auth.store";
 import { useSideBarStore } from "@/stores/sidebar.store";
@@ -6,19 +6,28 @@ import { Box, Text } from "@chakra-ui/react";
 import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    useAuthStore.getState().fetchUser();
+    useSideBarStore.getState().fetchProjects();
+  }, []);
 
   return (
     <MainLayout>
-      <Box ml={{ base: 0, md: "255px" }} w="100%" >
-        <Box height="100%" width="100%" display="flex" flexDir="column" alignItems="center" justifyContent="center">
-          <Text fontSize="2rem">
-            Welcome to WorkSpace
-          </Text>
+      <Box ml={{ base: 0, md: "255px" }} w="100%">
+        <Box
+          height="100%"
+          width="100%"
+          display="flex"
+          flexDir="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text fontSize="2rem">Welcome to WorkSpace</Text>
           <Text fontSize="1.5rem">
             Have a nice day. Hope you enjoy our journey
           </Text>
         </Box>
       </Box>
     </MainLayout>
-  )
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
-  const protectedRoutes = ["/"]
+  const protectedRoutes = ["/", "/projects"];
+
+  console.log(req.nextUrl.pathname);
 
   if (protectedRoutes.includes(req.nextUrl.pathname)) {
-    const isAuth = req.cookies.get("Authentication")?.value
+    const isAuth = req.cookies.get("Authentication")?.value;
     if (!isAuth) {
-      const url = new URL('/auth/login', req.nextUrl.origin)
-      return NextResponse.redirect(url.toString())
+      const url = new URL("/auth/login", req.nextUrl.origin);
+      return NextResponse.redirect(url.toString());
     }
   }
 }
